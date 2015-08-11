56721f5b4f81db841ed6dc1920f211775f9f830f
Mathias Fussenegger <f.mathias@zignar.net>
2015-08-11 13:53:02 +0200

    add a shared reader allocation which is used across phases

--- sql/src/main/java/io/crate/operation/collect/ShardCollectService.java
+++ sql/src/main/java/io/crate/operation/collect/ShardCollectService.java
@@ -21,6 +21,7 @@
 
 package io.crate.operation.collect;
 
+import io.crate.action.job.SharedShardContext;
 import io.crate.action.sql.query.CrateSearchContext;
 import io.crate.analyze.EvaluatingNormalizer;
 import io.crate.blob.v2.BlobIndices;
@@ -46,7 +47,6 @@ import org.elasticsearch.cluster.ClusterService;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.Singleton;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.index.IndexService;
 import org.elasticsearch.index.engine.Engine;
 import org.elasticsearch.index.mapper.MapperService;
 import org.elasticsearch.index.shard.IndexShard;
@@ -64,7 +64,6 @@ public class ShardCollectService {
     private final EvaluatingNormalizer shardNormalizer;
     private final ProjectionToProjectorVisitor projectorVisitor;
     private final boolean isBlobShard;
-    private final IndexService indexService;
     private final BlobIndices blobIndices;
 
     @Inject
@@ -75,7 +74,6 @@ public class ShardCollectService {
                                TransportActionProvider transportActionProvider,
                                BulkRetryCoordinatorPool bulkRetryCoordinatorPool,
                                ShardId shardId,
-                               IndexService indexService,
                                Functions functions,
                                ShardReferenceResolver referenceResolver,
                                BlobIndices blobIndices,
@@ -84,7 +82,6 @@ public class ShardCollectService {
         this.searchContextFactory = searchContextFactory;
         this.threadPool = threadPool;
         this.shardId = shardId;
-        this.indexService = indexService;
         this.blobIndices = blobIndices;
         isBlobShard = BlobIndices.isBlobShard(this.shardId);
 
@@ -174,8 +171,9 @@ public class ShardCollectService {
                                                    final JobCollectContext jobCollectContext,
                                                    final int jobSearchContextId,
                                                    int pageSize) throws Exception {
-        IndexShard indexShard = indexService.shardSafe(shardId.id());
-        Engine.Searcher searcher = EngineSearcher.getSearcherWithRetry(indexShard, "search", null);
+        SharedShardContext sharedShardContext = jobCollectContext.readerAllocation().getContext(shardId);
+        Engine.Searcher searcher = sharedShardContext.searcher();
+        IndexShard indexShard = sharedShardContext.indexShard();
         CrateSearchContext searchContext = null;
         try {
              searchContext = searchContextFactory.createContext(
