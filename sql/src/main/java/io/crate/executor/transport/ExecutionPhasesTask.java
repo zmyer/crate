46e827e5d0a264558cb5912dcb5259acc41b49a3
Sebastian Utz <su@rtme.net>
2016-04-07 14:37:39 +0200

    WIP: kill jobs on jobInit (preparation) error
    and only respond to client after all nodes
    responded to the kill request

--- sql/src/main/java/io/crate/executor/transport/ExecutionPhasesTask.java
+++ sql/src/main/java/io/crate/executor/transport/ExecutionPhasesTask.java
@@ -29,8 +29,12 @@ import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
 import io.crate.action.job.*;
 import io.crate.core.collections.Bucket;
+import io.crate.exceptions.Exceptions;
 import io.crate.executor.JobTask;
 import io.crate.executor.TaskResult;
+import io.crate.executor.transport.kill.KillJobsRequest;
+import io.crate.executor.transport.kill.KillResponse;
+import io.crate.executor.transport.kill.TransportKillJobsNodeAction;
 import io.crate.jobs.*;
 import io.crate.operation.*;
 import io.crate.operation.projectors.RowReceiver;
@@ -45,24 +49,26 @@ import org.elasticsearch.indices.IndicesService;
 
 import javax.annotation.Nullable;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
-public class ExecutionPhasesTask extends JobTask {
+class ExecutionPhasesTask extends JobTask {
 
     private final ESLogger logger;
     private final TransportJobAction transportJobAction;
     private final List<NodeOperationTree> nodeOperationTrees;
     private final OperationType operationType;
     private final ClusterService clusterService;
-    private ContextPreparer contextPreparer;
+    private final ContextPreparer contextPreparer;
     private final JobContextService jobContextService;
     private final IndicesService indicesService;
+    private final TransportKillJobsNodeAction transportKillJobsNodeAction;
 
 
     private final List<SettableFuture<TaskResult>> results = new ArrayList<>();
     private boolean hasDirectResponse;
 
-    public enum OperationType {
+    enum OperationType {
         BULK,
         /**
          * UNKNOWN means it will depend on the number of nodeOperationTrees, if > 1 it will use bulk, otherwise QueryResult
@@ -70,15 +76,16 @@ public class ExecutionPhasesTask extends JobTask {
         UNKNOWN
     }
 
-    protected ExecutionPhasesTask(UUID jobId,
-                                  ESLogger logger,
-                                  ClusterService clusterService,
-                                  ContextPreparer contextPreparer,
-                                  JobContextService jobContextService,
-                                  IndicesService indicesService,
-                                  TransportJobAction transportJobAction,
-                                  List<NodeOperationTree> nodeOperationTrees,
-                                  OperationType operationType) {
+    ExecutionPhasesTask(UUID jobId,
+                        ESLogger logger,
+                        ClusterService clusterService,
+                        ContextPreparer contextPreparer,
+                        JobContextService jobContextService,
+                        IndicesService indicesService,
+                        TransportJobAction transportJobAction,
+                        TransportKillJobsNodeAction transportKillJobsNodeAction,
+                        List<NodeOperationTree> nodeOperationTrees,
+                        OperationType operationType) {
         super(jobId);
         this.logger = logger;
         this.clusterService = clusterService;
@@ -86,6 +93,7 @@ public class ExecutionPhasesTask extends JobTask {
         this.jobContextService = jobContextService;
         this.indicesService = indicesService;
         this.transportJobAction = transportJobAction;
+        this.transportKillJobsNodeAction = transportKillJobsNodeAction;
         this.nodeOperationTrees = nodeOperationTrees;
         this.operationType = operationType;
 
@@ -115,15 +123,18 @@ public class ExecutionPhasesTask extends JobTask {
         List<PageDownstreamContext> pageDownstreamContexts = new ArrayList<>(nodeOperationTrees.size());
         List<Tuple<ExecutionPhase, RowReceiver>> handlerPhases = new ArrayList<>(nodeOperationTrees.size());
 
+        KillOnFailure killOnFailure = new KillOnFailure(jobId(), operationByServer.size(), results,
+                transportKillJobsNodeAction, logger);
+
         if (operationType == OperationType.BULK || nodeOperationTrees.size() > 1) {
             // bulk Operation with rowCountResult
             for (int i = 0; i < nodeOperationTrees.size(); i++) {
-                SettableFuture<TaskResult> result = results.get(i);
+                SettableFuture<TaskResult> result = killOnFailure.resultFutures.get(i);
                 RowCountResultRowDownstream rowDownstream = new RowCountResultRowDownstream(result);
                 handlerPhases.add(new Tuple<ExecutionPhase, RowReceiver>(nodeOperationTrees.get(i).leaf(), rowDownstream));
             }
         } else {
-            SettableFuture<TaskResult> result = Iterables.getOnlyElement(results);
+            SettableFuture<TaskResult> result = Iterables.getOnlyElement(killOnFailure.resultFutures);
             QueryResultRowDownstream downstream = new QueryResultRowDownstream(result);
             handlerPhases.add(new Tuple<ExecutionPhase, RowReceiver>(Iterables.getOnlyElement(nodeOperationTrees).leaf(), downstream));
         }
@@ -138,7 +149,7 @@ public class ExecutionPhasesTask extends JobTask {
         if (operationByServer.isEmpty()) {
             return;
         }
-        sendJobRequests(pageDownstreamContexts, operationByServer);
+        sendJobRequests(pageDownstreamContexts, operationByServer, killOnFailure);
     }
 
     private void setupContext(Map<String, Collection<NodeOperation>> operationByServer,
@@ -160,14 +171,17 @@ public class ExecutionPhasesTask extends JobTask {
         if (hasDirectResponse) {
             for (ExecutionSubContext executionSubContext : localContextAndStartOperation) {
                 assert executionSubContext != null && executionSubContext instanceof DownstreamExecutionSubContext
-                        : "Need DownstreamExecutionSubContext for DIRECT_RESPONSE remote jobs. Got " + executionSubContext;
+                        : "Need DownstreamExecutionSubContext for DIRECT_RESPONSE remote jobs. Got " +
+                          executionSubContext;
                 pageDownstreamContexts.add(((DownstreamExecutionSubContext) executionSubContext).pageDownstreamContext((byte) 0));
             }
         }
     }
 
     private void sendJobRequests(List<PageDownstreamContext> pageDownstreamContexts,
-                                 Map<String, Collection<NodeOperation>> operationsByServer) {
+                                 Map<String, Collection<NodeOperation>> operationsByServer,
+                                 KillOnFailure killOnFailure) {
+        logger.trace("operationsByServer={}", operationsByServer);
         int idx = 0;
         for (Map.Entry<String, Collection<NodeOperation>> entry : operationsByServer.entrySet()) {
             String serverNodeId = entry.getKey();
@@ -175,10 +189,10 @@ public class ExecutionPhasesTask extends JobTask {
 
             JobRequest request = new JobRequest(jobId(), nodeOperations);
             if (hasDirectResponse) {
-                transportJobAction.execute(serverNodeId, request, new DirectResponseListener(idx, pageDownstreamContexts, logger));
+                transportJobAction.execute(serverNodeId, request, new DirectResponseListener(idx, pageDownstreamContexts, killOnFailure, logger));
             } else {
                 transportJobAction.execute(serverNodeId, request,
-                        new FailureOnlyResponseListener(results, logger));
+                        new FailureOnlyResponseListener(logger, killOnFailure, idx));
             }
             idx++;
         }
@@ -186,7 +200,7 @@ public class ExecutionPhasesTask extends JobTask {
 
     /**
      * removes the localNodeId entry from the nodesByServer map and initializes the context and starts the operation.
-     *
+     * <p>
      * This is done in order to be able to create the JobExecutionContext with the localMerge PageDownstreamContext
      */
     private List<ExecutionSubContext> createLocalContextAndStartOperation(Map<String, Collection<NodeOperation>> operationsByServer,
@@ -238,11 +252,13 @@ public class ExecutionPhasesTask extends JobTask {
 
         private final int bucketIdx;
         private final List<PageDownstreamContext> pageDownstreamContexts;
+        private final KillOnFailure killOnFailure;
         private final ESLogger logger;
 
-        public DirectResponseListener(int bucketIdx, List<PageDownstreamContext> pageDownstreamContexts, ESLogger logger) {
+        DirectResponseListener(int bucketIdx, List<PageDownstreamContext> pageDownstreamContexts, KillOnFailure killOnFailure, ESLogger logger) {
             this.bucketIdx = bucketIdx;
             this.pageDownstreamContexts = pageDownstreamContexts;
+            this.killOnFailure = killOnFailure;
             this.logger = logger;
         }
 
@@ -269,6 +285,7 @@ public class ExecutionPhasesTask extends JobTask {
                     }
                 });
             }
+            killOnFailure.processNodeResponse(null);
         }
 
         @Override
@@ -276,32 +293,133 @@ public class ExecutionPhasesTask extends JobTask {
             for (PageDownstreamContext pageDownstreamContext : pageDownstreamContexts) {
                 pageDownstreamContext.failure(bucketIdx, e);
             }
+            killOnFailure.processNodeResponse(e);
         }
     }
 
     private static class FailureOnlyResponseListener implements ActionListener<JobResponse> {
 
-        private final List<SettableFuture<TaskResult>> results;
+        private final KillOnFailure killOnFailure;
         private final ESLogger logger;
+        private final int nodeIdx;
 
-        public FailureOnlyResponseListener(List<SettableFuture<TaskResult>> results, ESLogger logger) {
-            this.results = results;
+        FailureOnlyResponseListener(ESLogger logger, KillOnFailure killOnFailure, int nodeIdx) {
             this.logger = logger;
+            this.killOnFailure = killOnFailure;
+            this.nodeIdx = nodeIdx;
         }
 
         @Override
         public void onResponse(JobResponse jobResponse) {
+            Throwable throwable = null;
             if (jobResponse.directResponse().size() > 0) {
-                for (SettableFuture<TaskResult> result : results) {
-                    result.setException(new IllegalStateException("Got a directResponse but didn't expect one"));
-                }
+                throwable = new IllegalStateException("Got a directResponse but didn't expect one");
             }
+            killOnFailure.processNodeResponse(throwable);
         }
 
         @Override
         public void onFailure(Throwable e) {
-            // in the non-direct-response case the failure is pushed to downStreams
             logger.warn(e.getMessage(), e);
+            killOnFailure.processNodeResponse(e);
         }
     }
+
+    private static class KillOnFailure {
+
+        private final UUID jobId;
+        private final AtomicInteger nodesResponded;
+        private final List<SettableFuture<TaskResult>> resultFutures;
+        private final List<TaskResult> results;
+        private final List<SettableFuture<TaskResult>> finalResults;
+        private final AtomicInteger killResponses;
+        private final TransportKillJobsNodeAction transportKillJobsNodeAction;
+        private final ESLogger logger;
+
+        private Throwable rootCause;
+
+        KillOnFailure(UUID jobId,
+                      int nodesCnt,
+                      List<SettableFuture<TaskResult>> finalResults,
+                      TransportKillJobsNodeAction transportKillJobsNodeAction,
+                      ESLogger logger) {
+            this.jobId = jobId;
+            this.transportKillJobsNodeAction = transportKillJobsNodeAction;
+            this.logger = logger;
+            this.finalResults = finalResults;
+            results = new ArrayList<>(finalResults.size());
+            resultFutures = new ArrayList<>(finalResults.size());
+            for (int i = 0; i < finalResults.size(); i++) {
+                SettableFuture<TaskResult> future = SettableFuture.create();
+                resultFutures.add(future);
+            }
+            logger.trace("killOnFailure initialized with number_of_nodes={}", nodesCnt);
+            nodesResponded = new AtomicInteger(nodesCnt);
+            killResponses = new AtomicInteger(nodesCnt);
+        }
+
+        void processNodeResponse(@Nullable Throwable throwable) {
+            if (throwable != null) {
+                throwable = Exceptions.unwrap(throwable);
+                if (rootCause == null) {
+                    rootCause = throwable;
+                }
+            }
+            //logger.trace("Got node response from job init request (nodesResponded={}) with failure={}", nodesResponded.get(), throwable);
+
+            if (nodesResponded.decrementAndGet() == 0) {
+                if (rootCause != null) {
+                    // all nodes responded, kill the job
+                    logger.trace("All nodes responded, lets kill the job..");
+                    kill();
+                } else {
+                    logger.trace("All nodes responded successfully");
+                    finish();
+                }
+            }
+        }
+
+        private void kill() {
+            transportKillJobsNodeAction.executeKillOnAllNodes(
+                    new KillJobsRequest(Collections.singletonList(jobId)), new ActionListener<KillResponse>() {
+                        @Override
+                        public void onResponse(KillResponse killResponse) {
+                            logger.debug("Killed {} jobs", killResponse.numKilled());
+                            finishAfterKill();
+
+                        }
+
+                        @Override
+                        public void onFailure(Throwable e) {
+                            logger.warn("Failed to kill job", e);
+                            finishAfterKill();
+                        }
+                    }
+            );
+        }
+
+        private void finishAfterKill() {
+            if (killResponses.decrementAndGet() <= 0) {
+                logger.trace("Killed job, will set result exception now", rootCause);
+                finish();
+            }
+
+        }
+
+        private void finish() {
+            for (int i = 0; i < resultFutures.size(); i++) {
+                SettableFuture<TaskResult> finalResult = finalResults.get(i);
+                if (rootCause != null) {
+                    finalResult.setException(rootCause);
+                } else {
+                    try {
+                        finalResult.set(resultFutures.get(i).get());
+                    } catch (Exception e) {
+                        finalResult.setException(e);
+                    }
+                }
+            }
+        }
+    }
+
 }
--- sql/src/main/java/io/crate/executor/transport/TransportExecutor.java
+++ sql/src/main/java/io/crate/executor/transport/TransportExecutor.java
@@ -221,6 +221,7 @@ public class TransportExecutor implements Executor {
                     jobContextService,
                     indicesService,
                     transportActionProvider.transportJobInitAction(),
+                    transportActionProvider.transportKillJobsNodeAction(),
                     nodeOperationTrees,
                     operationType
             );
